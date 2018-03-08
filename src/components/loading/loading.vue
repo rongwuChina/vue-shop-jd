675ef3be2a6ecc7d76d8abe28e56a59470d72932
rongW <15871494583@163.com>
2018-03-08 19:37:00 +0800

    添加加载中 loading的组件
    loading...

---
+++ src/components/loading/loading.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="loading">
+        <p class="loadcss"> loading...</p>   
+   </div>
+</template>
+
+<script>
+ export default {
+     name:"loading"
+ }
+</script>
+
+<style>
+    .loadcss{
+    color:#8a8a8a;
+    transform:rotateX(90grd);
+    animation: opcity1 1.5s infinite alternate;
+    position:relative:
+    top:20%;
+    text-align:center;
+    }
+    @keyframes opcity1{
+      from{
+         opacity:1;
+      }to{
+          opacity:0.3;
+      }
+}
+</style>
